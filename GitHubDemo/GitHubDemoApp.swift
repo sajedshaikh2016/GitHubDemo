b5beaa1ea13373590c54105230f3b10a98a63b78
Sajed Shaikh <sajed.shaikh@hotmail.com>
2023-01-14 12:16:07 +0530

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Sajed Shaikh on 14/01/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
