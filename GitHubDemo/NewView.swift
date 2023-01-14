fd68f5d4e9959a32e816f450cbb79d1e1c70e82c
Sajed Shaikh <sajed.shaikh@hotmail.com>
2023-01-14 12:18:23 +0530

    NewView added.

---
+++ GitHubDemo/NewView.swift
@@ -0,0 +1,20 @@
+//
+//  NewView.swift
+//  GitHubDemo
+//
+//  Created by Sajed Shaikh on 14/01/23.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewView()
+    }
+}
