0c88c5640d0cc1388242b9c6aac4443edf7d23b8
Sajed Shaikh <sajed.shaikh@hotmail.com>
2023-01-14 17:22:56 +0530

    Removed padding.

--- GitHubDemo/ContentView.swift
+++ GitHubDemo/ContentView.swift
@@ -13,7 +13,6 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-                .padding()
             Text("Hello, world!")
                 .padding()
                 .foregroundColor(.red)
